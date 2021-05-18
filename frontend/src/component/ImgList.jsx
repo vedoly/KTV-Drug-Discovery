399e5b97deac568c967772dd8c8eca523a852cf1
velodica <ravipas.apk@gmail.com>
2021-05-19 03:28:53 +0700

    fix bugs

--- frontend/src/component/ImgList.jsx
+++ frontend/src/component/ImgList.jsx
@@ -9,22 +9,22 @@ export const ImgList = (props) => {
   const numbers = props.state.onChem.map((x) => +x);
   numbers.sort((a, b) => a - b);
   if (props.state.pageState !== "Loading") {
-    // return props.state.pageState === "Suggestion" ? (
-    return numbers.map((number) => (
-      <ui>
-        {/* {`RX_${number}:    \n`} */}
+    return props.state.pageState === "Suggestion" ? (
+      numbers.map((number) => (
+        <ui>
+          {/* {`RX_${number}:    \n`} */}
 
-        <Img
-          number={number}
-          result={resultChem[number - 1]}
-          state={props.state}
-          setState={props.setState}
-        ></Img>
-      </ui>
-    ));
-    // ) : (
-    //   <div>Select Smi</div>
-    // );
+          <Img
+            number={number}
+            result={resultChem[number - 1]}
+            state={props.state}
+            setState={props.setState}
+          ></Img>
+        </ui>
+      ))
+    ) : (
+      <div></div>
+    );
   } else {
     return (
       <div>
